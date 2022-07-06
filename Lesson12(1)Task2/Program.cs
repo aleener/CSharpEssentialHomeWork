ac8400a6e0c13611773cfa6bce63c0d401191b34
o.liner <alexleener@gmail.com>
2022-07-06 23:05:06 +0300

    Add project files.

---
+++ Lesson12(1)Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson12_1_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
