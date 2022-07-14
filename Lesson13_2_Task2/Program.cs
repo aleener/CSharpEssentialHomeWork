3ef380f69494484b3ad92b1c863ae3828b645ffa
o.liner <alexleener@gmail.com>
2022-07-14 22:38:06 +0300

    Lesson13_2_Task2

---
+++ Lesson13_2_Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13_2_Task2
+{
+    internal class Program
+    {
+        //    Создать класс Converter.
+        //    В теле класса создать пользовательский конструктор, который принимает три вещественных аргумента, 
+        //    и инициализирует поля соответствующие курсу 3-х основных валют,
+        //    по отношению к гривне – public Converter(double usd, double eur, double rub). 
+        //    Написать программу, которая будет выполнять конвертацию из гривны в одну из указанных валют, 
+        //    также программа должна производить конвертацию из указанных валют в гривну.
+        static void Main(string[] args)
+        {
+        }
+    }
+}
