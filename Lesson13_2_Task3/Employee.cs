afb1c587e026e767fe8bbeaf74902fba3bfb57bc
o.liner <alexleener@gmail.com>
2022-07-18 00:44:36 +0300

    start Lesson13_2_Task3

---
+++ Lesson13_2_Task3/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13_2_Task3
+{
+    internal class Employee
+    {
+        string firstName;
+        string lastName;
+
+        public Employee(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        
+    }
+}
