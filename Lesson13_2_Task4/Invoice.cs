f152bbe4f8ebe4342f055c8c15f84f58423b328f
o.liner <alexleener@gmail.com>
2022-07-18 22:09:59 +0300

    finished Lesson13_2_Task4

---
+++ Lesson13_2_Task4/Invoice.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13_2_Task4
+{
+    //Создать класс Invoice.
+    //В теле класса создать три поля int account, string customer, string provider,
+    //которые должны быть проинициализированы один раз (при создании экземпляра данного класса)
+    //без возможности их дальнейшего изменения.
+    //В теле класса создать два закрытых поля string article, int quantity
+    //Создать метод расчета стоимости заказа с НДС и без НДС.
+    internal class Invoice
+    {
+        int account { get; } //рахунок
+        string customer { get; } //клієнт
+        string provider { get; } //постачальник
+        string article; //товар
+        int quantity; //кількість
+        double price; //ціна
+
+        public string Article
+        { 
+            get { return article; }
+            set { article = value; }
+        }
+        
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+        public int Price
+        {
+            set { price = value; }
+        }
+        public Invoice(int account, string customer, string provider)
+        {
+            this.account = account;
+            this.customer = customer;
+            this.provider = provider;
+        }
+        private double totalPrice()
+        { 
+            return (double)price * Quantity;
+        }
+        private double totalPriceVATfree()
+        {
+            return (double)price * Quantity * 0.8;
+        }
+        public void Show()
+        {
+            Console.WriteLine(  $"Account:\t{this.account}\n" +
+                                $"Customer:\t{this.customer}\n" +
+                                $"Provider:\t{this.provider}\n" +
+                                $"Article:\t{this.article}\n" +
+                                $"Quantity:\t{this.quantity}\n" +
+                                $"{new string('-', 30)}\n" +
+                                $"Price VAT free:\t{totalPriceVATfree()} \u20B4\n" +
+                                $"Total price:\t{totalPrice()} \u20B4\n");
+        }
+    }
+}
--- Lesson13_2_Task4/Program.cs
+++ Lesson13_2_Task4/Program.cs
@@ -8,8 +8,24 @@ namespace Lesson13_2_Task4
 {
     internal class Program
     {
+        //Создать класс Invoice.
+        //В теле класса создать три поля int account, string customer, string provider,
+        //которые должны быть проинициализированы один раз (при создании экземпляра данного класса)
+        //без возможности их дальнейшего изменения.
+        //В теле класса создать два закрытых поля string article, int quantity
+        //Создать метод расчета стоимости заказа с НДС и без НДС.
+        //Написать программу, которая выводит на экран сумму оплаты заказанного товара с НДС или без НДС.
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Invoice order1 = new Invoice(1, "Ivanov Borys", "HARVEST");
+            order1.Article = "TRAVELER";
+            order1.Quantity = 2;
+            order1.Price = 1975;
+            order1.Show();
+
+            Console.ReadKey();
         }
     }
 }
