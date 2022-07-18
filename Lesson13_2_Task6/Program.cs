67df7b9d7c8b8b01fac14b5ace1abfb719efa2d3
o.liner <alexleener@gmail.com>
2022-07-18 23:21:08 +0300

    added and finished Lesson13_2_Task6

---
+++ Lesson13_2_Task6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13_2_Task6
+{
+    internal class Program
+    {
+        //Создать класс User, содержащий информацию о пользователе (логин, имя, фамилия, возраст, дата заполнения анкеты).
+        //Поле дата заполнения анкеты должно быть проинициализировано только один раз (при создании экземпляра данного класса) без возможности его дальнейшего изменения.
+        //Реализуйте вывод на экран информации о пользователе.
+        static void Main(string[] args)
+        {
+            User user1 = new User();
+            User user2 = new User("Br1tv@","Borys","Ivanov");
+            
+            user1.show();
+            Console.WriteLine(new String ('-',30));
+            user2.show();
+            Console.WriteLine(new String('-', 30));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson13_2_Task6/User.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson13_2_Task6
+{
+    //Создать класс User, содержащий информацию о пользователе (логин, имя, фамилия, возраст, дата заполнения анкеты).
+    //Поле дата заполнения анкеты должно быть проинициализировано только один раз (при создании экземпляра данного класса) без возможности его дальнейшего изменения.
+    //Реализуйте вывод на экран информации о пользователе.
+    internal class User
+    {
+        string login;
+        string firstName;
+        string lastName;
+        DateTime createDate = DateTime.Now;
+
+        public User() { }
+        public User(string login, string firstName, string lastName)
+        {
+            this.login = LoginNotNull(login);
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        //перевірка запису значення до поля Login, не Null і не пробіли
+        private string LoginNotNull(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Login must not be blank");
+            return value;
+        }
+
+        public void show()
+        {
+            Console.WriteLine($"Login:\t\t{this.login}\n" +
+                    $"First name:\t{this.firstName}\n" +
+                    $"Last name:\t{this.lastName}\n" +
+                    $"Create date:\t{this.createDate}\n");
+        }
+    }
+}
