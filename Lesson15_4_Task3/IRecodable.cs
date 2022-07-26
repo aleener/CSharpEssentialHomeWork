6ed365b0a5865cba167d0bf23fac5956fd544757
o.liner <alexleener@gmail.com>
2022-07-26 13:54:50 +0300

    finished Lesson15_4_Task3

---
+++ Lesson15_4_Task3/IRecodable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson15_4_Task3
+{
+    internal interface IRecodable
+    {
+        void Play();
+        void Pause();
+        void Stop();
+    }
+}
---
+++ Lesson15_4_Task3/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson15_4_Task3
+{
+    internal class Player : IPlayable,IRecodable
+    {
+        public void Play() 
+        {
+            Console.WriteLine("Plying");
+        }
+        public void Record()
+        {
+            Console.WriteLine("Recording");
+        }
+        public void Pause()
+        {
+            Console.WriteLine("Paused");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Stopped");
+        }
+    }
+}
