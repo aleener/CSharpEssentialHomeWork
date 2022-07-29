33ab3f8b8fc608ffa826f249b7ef8d36c8f148ca
o.liner <alexleener@gmail.com>
2022-07-29 11:50:19 +0300

    finished Lesson16_5_Task2

---
+++ Lesson16_5_Task2/Array.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16_5_Task2
+{
+    internal class Array
+    {
+        Random random = new Random();
+
+        private int[] array;
+
+        public Array(int value)
+        {
+            array = new int[value];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+        public void RandomFill()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 9);
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        public int MaxValue()
+        {
+            int max = default(int);
+
+            max = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+
+            }
+            return max;
+        }
+        public int MinValue()
+        {
+            int min = default(int);
+
+            min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > min)
+                {
+                    continue;
+                }
+                else
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+        public int SumOfElements()
+        {
+            int sum = default(int);
+
+            sum = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+        public double AverageOfElements()
+        {
+            double result = 0;
+            for (int i = 0; i < array.Length; i++)
+                result += array[i];
+
+            return result / array.Length;
+        }
+        public void PrintOddValues()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((array[i] & 1) != 0)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+                
+            }
+            Console.WriteLine();
+        }
+    }   
+}
+/* if (i < array.Length)
+ {
+     Console.Write(array[i]);
+     Console.Write(",");
+ }
+ else
+ {
+     Console.Write(";");
+ }
+*/
---
+++ Lesson16_5_Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16_5_Task2
+{
+    //Требуется: Создать массив размерностью N элементов, заполнить его произвольными целыми значениями.
+    //Вывести наибольшее значение массива, наименьшее значение массива, общую сумму элементов, среднее арифметическое всех элементов, вывести все нечетные значения. 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.Write("Enter size of array: ");
+            int size = int.Parse(Console.ReadLine());
+
+            Array array = new Array(size);
+
+            array.RandomFill();
+
+            Console.Write("All values of {0} :\t\t", nameof(array));
+            array.Print();
+            Console.WriteLine("Max value of array is:\t\t{0}", array.MaxValue());
+            Console.WriteLine("Min value of array is:\t\t{0}", array.MinValue());
+            Console.WriteLine("Sum of all array values is:\t{0}", array.SumOfElements());
+            Console.WriteLine("Arithmetic sum is:\t\t{0}", array.AverageOfElements());
+            Console.Write("All array odd values:\t\t");
+            array.PrintOddValues();
+
+            Console.ReadKey();
+        }
+    }
+}
