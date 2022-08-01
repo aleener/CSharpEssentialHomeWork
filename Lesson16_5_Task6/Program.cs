1f0b441d890b4f5295d1bceb7c0b21f718a23721
o.liner <alexleener@gmail.com>
2022-08-01 12:32:36 +0300

    finished Lesson16_5_Task6

---
+++ Lesson16_5_Task6/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson16_5_Task6
+{
+    //Расширьте пример 5 (русско-английский словарь) еще и украинским словарем.
+    //Реализуйте возможность поиска не только по ключевым русским словам и словам на остальных языках.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Dictionary dictionary = new Dictionary();
+
+            Console.WriteLine(dictionary["sun"]);
+            Console.WriteLine(dictionary["дом"]);
+            Console.WriteLine(dictionary["ручка"]);
+            Console.WriteLine(dictionary["стіл"]);
+            Console.WriteLine(dictionary["карандаш"]);
+            Console.WriteLine(dictionary["apple"]);
+            Console.WriteLine(dictionary["солнце"]);
+
+            Console.WriteLine(new string('-', 20));
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
