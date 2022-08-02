b6cdbf04e2e88bce56dea4e790e6336bf55dacfd
o.liner <alexleener@gmail.com>
2022-08-02 11:53:38 +0300

    added Lesson17_6_Task4

---
+++ Lesson17_6_Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson17_6_Task4
+{
+    //Требуется: создать расширяющий метод для целочисленного массива, который сортирует элементы массива по возрастанию. 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
