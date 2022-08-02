e4ac5b174841680bd62826c41b4d962897f70e12
o.liner <alexleener@gmail.com>
2022-08-02 19:39:27 +0300

    finished Lesson17_6_Task6

---
+++ Lesson17_6_Task6/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson17_6_Task6
+{
+    static class Calculator
+    {
+        public static int Add(int operand1, int operand2)
+        {
+            return operand1 + operand2;
+        }
+        public static int Sub(int operand1, int operand2)
+        {
+            return operand1 - operand2;
+        }
+        public static int Mul(int operand1, int operand2)
+        {
+            return operand1 * operand2;
+        }
+        public static void Div(int operand1, int operand2)
+        {
+            if (operand2 != 0)
+            {
+                double div = (double)operand1 / operand2;
+                Console.WriteLine("{0} / {1} = {2}\n", operand1, operand2, div);
+            }
+            else
+            {
+                Console.WriteLine("You can't divide by zero");
+            }
+        }
+    }
+}
---
+++ Lesson17_6_Task6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson17_6_Task6
+{
+    //Требуется: Создать статический класс Calculator, с методами для выполнения основных арифметических операций.
+    //Написать программу, которая выводит на экран основные арифметические операции.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A,B;
+
+            Console.Write("Enter number A: ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("Enter number B: ");
+            B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{A} + {B} = {Calculator.Add(A, B)}\n");
+            Console.WriteLine($"{A} - {B} = {Calculator.Sub(A, B)}\n");
+            Console.WriteLine($"{A} * {B} = {Calculator.Mul(A, B)}\n");
+            Calculator.Div(A, B);
+
+            Console.ReadKey();
+        }
+    }
+}
