09a0052972a289ad453c046edade0111e4da2791
o.liner <alexleener@gmail.com>
2022-08-02 11:52:11 +0300

    finished Lesson17_6_Task3

--- Lesson17_6_Task3/Program.cs
+++ Lesson17_6_Task3/Program.cs
@@ -7,10 +7,31 @@ using System.Threading.Tasks;
 namespace Lesson17_6_Task3
 {
     //Расширьте пример урока 005_Delegation, создав в классе Book, вложенный класс Notes, который позволит сохранять заметки читателя. 
-    internal class Program
+    class Book
+    {
+        private static string note = "";
+        public class Notes
+        {
+            public void NoteAdd(string value)
+            { 
+                note = value;
+                Console.WriteLine(note);
+            }
+        }
+        public void FindNext(string str)
+        {
+            Console.WriteLine("Find string : " + str);
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
+            Book.Notes instance = new Book.Notes();
+
+            instance.NoteAdd("Add note");
+
+            Console.ReadKey();
         }
     }
 }
