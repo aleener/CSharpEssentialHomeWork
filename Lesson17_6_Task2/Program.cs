66cfb2f9e2d3006c530cfd3a8a709ece4d36e69e
o.liner <alexleener@gmail.com>
2022-08-02 11:09:21 +0300

    finished Lesson17_6_Task2

--- Lesson17_6_Task2/Program.cs
+++ Lesson17_6_Task2/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace Lesson17_6_Task2
 {
-    internal class Program
+    //Требуется: Создать статический класс FindAndReplaceManager с методом void FindNext(string str) для поиска по книге из примера урока 005_Delegation.
+    //При вызове этого метода, производится последовательный поиск строки в книге. 
+
+    static class FindAndReplaceManager
+    {
+        public static void FindNext(string str)
+        {
+            Book.FindNext(str);
+        }
+    }
+    static class Book
+    {
+        public static void FindNext(string str)
+        {
+            Console.WriteLine("Поиск строки : " + str);
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
+
+            FindAndReplaceManager.FindNext("from static");
+
+            Console.ReadKey();
         }
     }
 }
