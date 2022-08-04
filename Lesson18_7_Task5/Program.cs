4fb49912050185836642f0f69d41a2b9d1554de1
o.liner <alexleener@gmail.com>
2022-08-04 10:52:11 +0300

    finished Lesson18_7_Task5

---
+++ Lesson18_7_Task5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson18_7_Task5
+{
+    //Создайте структуру с именем - Notebook. Поля структуры: модель, производитель, цена.
+    //В структуре должен быть реализован конструктор для инициализации полей и метод для вывода содержимого полей на экран.
+    struct Notebook
+    {
+        string model;
+        string provider;
+        double price;
+
+        public Notebook(string model, string provider, double price)
+        { 
+            this.model = model;
+            this.provider = provider;
+            this.price = price;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Model: {this.model}\nProvider: {this.provider}\nPrice: {this.price}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook1 = new Notebook("Preadator", "ASUS", 35450);
+
+            notebook1.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
