8e2b3571d3e34250bf513674694738b9b2e356e3
o.liner <alexleener@gmail.com>
2022-08-15 16:06:23 +0300

    finished Lesson20_9_Task5

---
+++ Lesson20_9_Task5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson20_9_Task5
+{
+    // Создайте анонимный метод, который принимает в качестве параметров три целочисленных аргумента и возвращает среднее арифметическое этих аргументов.
+
+    public delegate double Calc(int a, int b, int c);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3, y = 5, z = 4;
+
+            Calc noname = (a, b, c) => (double)(a + b + c) / 3;
+
+            Console.WriteLine(noname(x,y,z));
+
+            Console.ReadKey();
+
+        }
+    }
+}
