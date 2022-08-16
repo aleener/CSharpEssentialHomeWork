72ba85142c3920fe37a964b515f87ce8d6d390c6
o.liner <alexleener@gmail.com>
2022-08-16 14:22:07 +0300

    add Lesson21_10_Task2

---
+++ Lesson21_10_Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson21_10_Task2
+{
+    //Создайте класс MyList.
+    //Реализуйте в простейшем приближении возможность использования его экземпляра аналогично экземпляру класса List.
+    //Минимально требуемый интерфейс взаимодействия с экземпляром, должен включать метод добавления элемента,
+    //индексатор для получения значения элемента по указанному индексу
+    //и свойство только для чтения для получения общего количества элементов.
+
+    class MyList
+    { 
+    
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
