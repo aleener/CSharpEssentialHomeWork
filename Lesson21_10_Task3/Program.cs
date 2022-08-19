77ceac55ccf1cf02567abd61f4bfffdc4bb5c0bf
o.liner <alexleener@gmail.com>
2022-08-19 15:16:56 +0300

    finished Lesson21_10_Task3

--- Lesson21_10_Task3/Program.cs
+++ Lesson21_10_Task3/Program.cs
@@ -12,12 +12,59 @@ namespace Lesson21_10_Task3
     //Используйте ограничения типа - интерфейс со свойством, хранящим тип существа, которое пытается получить подарок из мешка.
     internal class Program
     {
-        class MagigBag<T>
-        { 
-            
+        class MagicBag<T>
+        {
+            public T value;
+            private bool option = false;
+            private string type;
+
+            public MagicBag(T value)
+            {
+                this.value = value;
+                this.type = value.GetType().ToString();
+            }
+            public void GetPresent()
+            {
+                if (option == true)
+                    Console.WriteLine("{0}, you have already received a gift", value.GetType());
+                else if (type == "System.Int32" && option == false)
+                {
+                    Console.WriteLine("{0}, take your present", value.GetType());
+                    option = true;
+                }
+                else if (type == "System.Double" && option == false)
+                {
+                    Console.WriteLine("{0}, take your present", value.GetType());
+                    option = true;
+                }
+                else if (type == "System.String" && option == false)
+                {
+                    Console.WriteLine("{0}, take your present", value.GetType());
+                    option = true;
+                }
+
+            }
+
         }
         static void Main(string[] args)
         {
+            MagicBag<int> bag1 = new MagicBag<int>(1);
+            bag1.GetPresent();
+            bag1.GetPresent();
+
+            MagicBag<double> bag2 = new MagicBag<double>(1.3);
+            bag2.GetPresent();
+            bag2.GetPresent();
+            bag2.GetPresent();
+
+            MagicBag<string> bag3 = new MagicBag<string>("bag");
+            bag3.GetPresent();
+            bag3.GetPresent();
+            bag3.GetPresent();
+            bag3.GetPresent();
+
+            Console.ReadKey();
+
         }
     }
 }
